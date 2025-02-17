52dba944008ec7c5629fb5f3934bd69d3167c679
Petter <lacerdadominguesp@gmail.com>
2025-02-16 23:39:21 -0300

    Design simples
    Implementaçao de design basico e limpeza dos arquivos

--- Loja-de-Jogos/src/App.jsx
+++ Loja-de-Jogos/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//imports do projeto
+import React, {useState} from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
+//Funçao de estrutura
 function App() {
-  const [count, setCount] = useState(0)
+
+ const [values,setValues] = useState();
 
   return (
     <>
-      <div>
+      <div className='app-container'>
+        <div className='Register--container'>
+          <h1 className='Register--title'>Game Space</h1>
         
+          <input
+          type="text" 
+          name="name" 
+          placeholder='Nome'
+          />
+          <input
+          type="text" 
+          name="cost" 
+          placeholder='Preço' 
+          className='Register--input' 
+          />
+          <input
+          type="text" 
+          name="category" 
+          placeholder='Categoria' 
+          className='Register--input' 
+          />
+          <button className='Register--button'>Cadastrar</button>
+          </div>
       </div>
     </>
   )
